44b609520166471ff7c792c46a97db270dd109bd
zekalarcon <zekalarcon@gmail.com>
2020-11-23 12:30:12 -0300

    Update name and email

--- ejercicio_profundizacion_2.py
+++ ejercicio_profundizacion_2.py
@@ -11,9 +11,9 @@ Programa creado para que practiquen los conocimietos
 adquiridos durante la semana
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
-__version__ = "1.2"
+__author__ = "Ezequiel Alarcon"
+__email__ = "zekalarcon@gamil.com"
+
 
 
 import csv
